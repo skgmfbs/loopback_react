76da7019a5d82e77a0d3bb4f5aca10535a27092b
skgmfbs <skgmfbs@gmail.com>
2018-07-07 15:34:28 +0700

    create recorder model and make the relation with memo

--- common/models/saving-memo.js
+++ common/models/saving-memo.js
@@ -1,5 +1,19 @@
 'use strict';
 
-module.exports = function(Savingmemo) {
-
+module.exports = function (Savingmemo) {
+    Savingmemo.beforeRemote('create', function (context, user, next) {
+        context.args.data.createdDate = Date.now();
+        context.args.data.recorderId = context.req.accessToken.userId;
+        next();
+    });
+    Savingmemo.beforeRemote('find', function (context, user, next) {
+        if (context.args.filter === undefined) {
+            context.args.filter = {
+                where: {
+                    recorderId: context.req.accessToken.userId
+                }
+            };
+        }
+        next();
+    });
 };
